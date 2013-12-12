55501607355fe1a3b2daee2aaf07cc9a660f56e4
filosi <filosi@fbk.eu>
2013-12-12 12:03:24 +0100

    Parameter check into compute class

--- engine/scripts/compute_netdist.py
+++ engine/scripts/compute_netdist.py
@@ -7,11 +7,9 @@ import rpy2.rinterface as ri
 import numpy as np
 
 
-
 class NetDist:
     
     def __init__(self, filelist, param={}):
-        
         self.nfiles = len(filelist)
         try:
             self.nfiles > 2
@@ -19,29 +17,37 @@ class NetDist:
             print "Not enough file loaded"
             
         self.filelist = filelist
-        self.nettools = importr('nettools')
         self.mylist = rlc.TaggedList([])
         self.param = param
         
     def loadfiles(self):
+        """
+        Load files into R environment
+        """
         rcount = 0
         asmatrix = robjects.r['as.matrix']
         
-        param = {'sep':'\t', 'header': True, 'as_is':True, 'row.names': False}
-
-        for p in param.keys():
-            if self.param.has_key(p):
-                param[p] = self.param[p]
-                
+        ## Set the default parameter for reading from csv
+        param = {'sep':'\t', 'header':True, 'as_is':True, 'row.names':ri.NULL}
         
+        ## Check the correct parameter and set the default        
+        for p in param.keys():
+            if p in self.param:
+                if self.param is not None:
+                    param[p] = self.param[p]
+                
         for f in self.filelist:
             try:
-                dataf = DataFrame.from_csvfile(f, sep = "\t", header = True, as_is=True ,row_names=1)
+                dataf = DataFrame.from_csvfile(f, 
+                                               sep = param['sep'],
+                                               header = param['header'], 
+                                               as_is = param['as_is'] ,
+                                               row_names=param['row.names'])
                 dataf = asmatrix(dataf)
                 self.mylist.append(dataf)
                 rcount += 1
             except IOError:
-                print "Error in loading file %s" %f
+                print "Can't load file %s" %f
         
         if rcount == self.nfiles:
             return True
@@ -50,19 +56,31 @@ class NetDist:
 
 
     def compute(self):
+        """
+        Compute the distances between adjacency matrices using nettools
+        package
+
+        """
+        
+        nettools = importr('nettools')
         ## robjects.conversion.py2ri = numpy2ri
         param = {'d':'HIM', 'ga':ri.NULL, 'components':True, 'rho':1}
         
+        ## Check the correct parameter and set the default
         for p in param.keys():
-            if self.param.has_key(p):
-                param[p] = self.param[p]
+            if p in self.param:
+                if self.param is not None:
+                    param[p] = self.param[p]
+        
         try:
-            self.res = self.nettools.netdist(self.mylist, d=param['d'], components = param['components'], ga=param['ga'])
+            self.res = nettools.netdist(self.mylist,
+                                        d=param['d'], 
+                                        components = param['components'], 
+                                        ga=param['ga'], **{'n.cores':1})
             return_value = True
         except ValueError:
             print 'Error in computing network distance'
             return_value = False
-        
         
         self.computed = return_value
         return return_value
