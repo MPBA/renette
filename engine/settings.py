216950d2ffb8d5fa76b95c04628e14eb606748f4
Hiromu Yakura <hiromu1996@gmail.com>
2014-10-24 23:38:55 +0900

    Separate settings of celery and pgsql in tasks.py

---
+++ engine/settings.py
@@ -0,0 +1,8 @@
+BROKER_URL = 'redis://localhost:6379/0'
+BACKEND_URL = 'redis://localhost:6379/0'
+
+DBNAME = 'renette'
+DBUSER = 'renette'
+DBPASSWORD = 'nette@re!!'
+DBHOST = 'geopg'
+DBPORT = 50003
